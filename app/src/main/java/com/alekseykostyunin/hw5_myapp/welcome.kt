668b9d2893a743feef145ee6bc892a01c70bb5c7
AlekseyKostyunin <kostunin-ag@mail.ru>
2024-06-23 20:19:49 +0500

    Initial commit

---
+++ app/src/main/java/com/alekseykostyunin/hw5_myapp/welcome.kt
@@ -0,0 +1,28 @@
+package com.alekseykostyunin.hw5_myapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.alekseykostyunin.hw5_myapp.databinding.ActivityWelcomeBinding
+
+class welcome : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+
+
+}
